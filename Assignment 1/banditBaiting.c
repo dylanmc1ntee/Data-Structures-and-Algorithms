3f17eb79ffd4b0aec8914bb3b047a5228c45c575
dylanmc1ntee <dylangmcintee@gmail.com>
2024-01-15 22:29:23 -0500

    brainstorm

--- Assignment 1/banditBaiting.c
+++ Assignment 1/banditBaiting.c
@@ -12,6 +12,7 @@ typedef struct {
     
     char locationName[20];
     placement_t existingPlacements[1];
+    int numID;
     int totalFood;
 
 } location_t;
@@ -22,17 +23,81 @@ int main()
     int id, foodLevel;
     int end = 0;
 
-    while(end == 0)
+    int length = 1;
+    location_t *locations = (malloc(sizeof(location_t) * length));
+
+    if(locations == NULL)
     {
-        if((!(strcmp(name, "END") == 0) && (id == -1) && (foodLevel == -1)))
+        printf("Memory allocation FAILED.");
+    }
+    else
+    {
+        while(end == 0)
         {
+            scanf("%s %d %d", name, &id, &foodLevel);
+
+            if((!(strcmp(name, "END") == 0)) && (!(id == -1)) && (!(foodLevel == -1)))
+            {
+                int match = 0;
+
+                for(int i = 0; i < length; i++)
+                {
+                    if(strcmp(name, locations[i].locationName) == 0)
+                    {
+                        match = 1;
+
+                        for(int j = 0; j < locations[i].numID; j++)
+                        {
+                            if(locations[i].existingPlacements[j].id == id)
+                            {
+                                int changeInFood = locations[i].existingPlacements[j].previousFoodLevel - foodLevel;
+
+                                if(changeInFood > 0)
+                                {
+                                    printf("%d\n", changeInFood);
+                                    locations[i].existingPlacements[j].previousFoodLevel = foodLevel;
+                                    locations[i].totalFood += changeInFood;
+                                }
+                                else
+                                {
+                                    printf("0\n");
+                                    locations[i].existingPlacements[j].previousFoodLevel = foodLevel;
+                                }
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                    }
+                    if(match == 0)
+                    {
+                        if((sizeof(locations) / sizeof(locations[0])) == length)
+                        {
+                            length++;
+                            locations = realloc(locations, sizeof(locations) * length);
+                            
+                            
+                        }
+                    }
+                    match = 0;
+                }
             
-        }
-        else
-        {
-            end = 1;
+            }
+            else
+            {
+                for(int i = 0; i < length; i++)
+                {
+                    printf("%s %d\n", locations[i].locationName, locations[i].totalFood);
+                }
+
+                end = 1;
+            }
         }
     }
+
+    free(locations);
+    locations = NULL;
 
     return 0;
 }
