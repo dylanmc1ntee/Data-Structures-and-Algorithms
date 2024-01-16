f64663de3deddc63e7e3e06644e20a7eaf623b9a
dylanmc1ntee <dylangmcintee@gmail.com>
2024-01-16 17:03:06 -0500

    lab 1

---
+++ Labs/forbidden.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char origionalString[100 + 1]; 
+    scanf("%s", origionalString);
+
+    int numForbidden;
+    scanf("%d", &numForbidden);
+
+    char forbiddenChars[numForbidden];
+
+    for(int i = 0; i < numForbidden; i++)
+    {
+        scanf(" ");
+        scanf("%c", &forbiddenChars[i]);
+    }
+
+    int origionalLen = strlen(origionalString);
+
+    for(int i = 0; i < origionalLen; i++)
+    {
+        int remove = 0;
+
+        for(int j = 0; j < numForbidden; j++)
+        {
+            if(origionalString[i] == forbiddenChars[j])
+            {
+                remove = 1;
+                break;
+            }
+        }
+
+        if(remove == 0)
+        {
+            printf("%c", origionalString[i]);
+        }
+    }
+
+    return 0;
+}
