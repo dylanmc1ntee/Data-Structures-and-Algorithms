478850b98fa837497681e6815459ed867cc4da9c
dylanmc1ntee <dylangmcintee@gmail.com>
2024-01-30 17:00:13 -0500

    unfinished

--- Labs/Lab 3/linkedLists.c
+++ Labs/Lab 3/linkedLists.c
@@ -1,8 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+typedef struct{
+
+    int data;
+    Node * next;
+
+} Node;
+
+Node * removeAll(int value, Node * head)
+{
+    Node * newHead = head;
+    Node * prevNode;
+    Node * tmp;
+
+    while(newHead != NULL && newHead->data == value)
+    {
+        tmp = newHead;
+        newHead = newHead->next;
+        free(tmp);
+    }
+
+    prevNode = newHead;
+
+    while(prevNode != NULL && prevNode->next != NULL)
+    {
+        if(prevNode->next->data == value)
+        {
+            tmp = prevNode->next;
+            prevNode->next = tmp->next;
+            free(tmp);
+        }
+        else
+        {
+            prevNode = prevNode->next;
+        }
+    }
+
+    return newHead;
+}
 
 int main()
 {
+    Node * head = NULL;
+    Node * tail = NULL;
 
     return 0;
 }
