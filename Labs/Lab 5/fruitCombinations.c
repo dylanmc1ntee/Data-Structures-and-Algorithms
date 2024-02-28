3c4dcb6a9c37e826a72d380ca19e5728a1a02f93
dylanmc1ntee <dylangmcintee@gmail.com>
2024-02-27 21:15:02 -0500

    correct lab

--- Labs/Lab 5/fruitCombinations.c
+++ Labs/Lab 5/fruitCombinations.c
@@ -1,23 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define numFruit 9
-#define numBuy 8
+#define numFruit 7
+#define numBuy 3
 
-void combination(char ** allFruit, char ** basket, int basketIndex, int fruitIndex)
-{
+void combination(char ** allFruit, char ** basket, int fruitIndex, int basketIndex){
     static int count = 1;
-
-    if(basketIndex == numBuy) // filled basket
-    {
+    
+    // base case
+    if(basketIndex == numBuy) { // filled basket
         printf("%d. ", count);
-
-        for(int i = 0; i < numBuy; i++)
-        {
+        for(int i = 0; i < numBuy; i++) {
             printf("%s ", basket[i]);
         }
-
         count++;
         printf("\n");
         return;
@@ -25,28 +21,25 @@ void combination(char ** allFruit, char ** basket, int basketIndex, int fruitInd
 
     if(fruitIndex == numFruit) // out of fruit
     {
-        return;
+        return; // no more fruit :[ not enough to fill basket
     }
 
     basket[basketIndex] = allFruit[fruitIndex];
-    combination(allFruit, basket, fruitIndex + 1, basketIndex + 1); //buy the fruit
-    combination(allFruit, basket, fruitIndex + 1, basketIndex); // ignore the fruit, dont buy
+    combination(allFruit, basket, fruitIndex + 1, basketIndex + 1); // buy the fruit
+    combination(allFruit, basket, fruitIndex + 1, basketIndex); // dont buy the fruit
 }
 
-int main()
-{
-    char ** allFruit = (char**) malloc(sizeof(char *) * numFruit);
-    char ** basket = (char**) malloc(sizeof(char *) * numBuy);
+int main() {
+    char ** allFruit =(char **) malloc(sizeof(char*) * numFruit);
+    char ** basket = (char **) malloc(sizeof(char*) * numBuy);
 
     allFruit[0] = "apple";
-    allFruit[1] = "banana";
-    allFruit[2] = "cantaloupe";
-    allFruit[3] = "lemon";
-    allFruit[4] = "lime";
-    allFruit[5] = "apricot";
-    allFruit[6] = "plum";
-    allFruit[7] = "grape";
-    allFruit[8] = "cucumber";
+    allFruit[1] = "grape";
+    allFruit[2] = "orange";
+    allFruit[3] = "grapefruit";
+    allFruit[4] = "kiwi";
+    allFruit[5] = "tomato";
+    allFruit[6] = "banana";
 
     combination(allFruit, basket, 0, 0);
 
