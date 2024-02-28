3264f393af981c171468f901ed8743e5c46afd17
dylanmc1ntee <dylangmcintee@gmail.com>
2024-02-27 21:15:15 -0500

    failed attempt

--- Assignment 3/cozyLevel.c
+++ Assignment 3/cozyLevel.c
@@ -4,19 +4,28 @@
 
 #define numFamily 3
 
-void findMaxHappy(int arr1Cat, int arr1Fam, int arr2Cat1, int arr2Cat2, int initialHappiness[arr1Cat][arr1Fam], int relationship[arr2Cat1][arr2Cat2])
+void findMaxHappy(int ** initialHappiness, int ** relationship, int ** combo, int numCats)
 {
-    
+    for(int i = 0; i < numCats)
+    {
+        
+    }
 }
 
 int main()
 {
+    // get the number of cats
     int numCats;
     scanf("%d", &numCats);
 
-    int initialHappiness[numCats][numFamily];
+    /*
+        Create an array to hold the initial happiness levels of the
+        cats, and then gets those levels from the user and store it
+    */
+    int ** initialHappiness = (int **) malloc(sizeof(int *) * numFamily);
     for(int i = 0; i < numFamily; i++)
     {
+        initialHappiness[i] = (int *) malloc(sizeof(int) * numCats);
         for(int j = 0; j < numCats; j++)
         {
             int happy;
@@ -25,9 +34,14 @@ int main()
         }
     }
 
-    int relationship[numCats][numCats];
+    /*
+        Create an array to hold the relationships that the cats have
+        with eachother, gets those values from the user and store it
+    */
+    int ** relationship = (int **) malloc(sizeof(int *) * numCats);
     for(int i = 0; i < numCats; i++)
     {
+        relationship[i] = (int *) malloc(sizeof(int) * numCats);
         for(int j = 0; j < numCats; j++)
         {
             int happy;
@@ -35,6 +49,28 @@ int main()
             relationship[i][j] = happy;
         }
     }
+
+    int ** combo = (int **) malloc(sizeof(int *) * numFamily);
+    for(int i = 0; i < numFamily; i++)
+    {
+        combo[i] = (int *) malloc(sizeof(int) * numCats);
+    }
+
+    findMaxHappy(initialHappiness, relationship, combo, numCats);
+
+    // Free initialHappiness Array
+    for(int i = 0; i < numFamily; i++)
+    {
+        free(initialHappiness[i]);
+    }
+    free(initialHappiness);
+
+    // Free relationship Array
+    for(int i = 0; i < numCats; i++)
+    {
+        free(relationship[i]);
+    }
+    free(relationship);
 
     return 0;
 }
