55dedf9fb975e719c4d013639f73e2ed55f8b188
dylanmc1ntee <dylangmcintee@gmail.com>
2024-02-27 19:26:18 -0500

    static attempt

--- Assignment 3/cozyLevel.c
+++ Assignment 3/cozyLevel.c
@@ -1,48 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+#define numFamily 3
+
+void findMaxHappy(int arr1Cat, int arr1Fam, int arr2Cat1, int arr2Cat2, int initialHappiness[arr1Cat][arr1Fam], int relationship[arr2Cat1][arr2Cat2])
+{
+    
+}
 
 int main()
 {
     int numCats;
-    int numFamilies = 3;
-
     scanf("%d", &numCats);
-    
-    int ** origionalHappyLevel[numCats][numFamilies];
-    int ** happyWithOthers[numCats][numCats];
 
-    for(int i = 0; i < numCats; i++)
+    int initialHappiness[numCats][numFamily];
+    for(int i = 0; i < numFamily; i++)
     {
-        for(int j = 0; j < numFamilies; j++)
+        for(int j = 0; j < numCats; j++)
         {
-            scanf("%d", origionalHappyLevel[i][j]);
+            int happy;
+            scanf("%d", &happy);
+            initialHappiness[i][j] = happy;
         }
     }
 
+    int relationship[numCats][numCats];
     for(int i = 0; i < numCats; i++)
     {
         for(int j = 0; j < numCats; j++)
         {
-            scanf("%d", happyWithOthers[i][j]);
+            int happy;
+            scanf("%d", &happy);
+            relationship[i][j] = happy;
         }
-    }
-
-    for(int i = 0; i < numCats; i++)
-    {
-        for(int j = 0; j < numFamilies; j++)
-        {
-            printf("%d ", origionalHappyLevel[i][j]);
-        }
-        printf("\n");
-    }
-
-    for(int i = 0; i < numCats; i++)
-    {
-        for(int j = 0; j < numCats; j++)
-        {
-            printf("%d ", happyWithOthers[i][j]);
-        }
-        printf("\n");
     }
 
     return 0;
