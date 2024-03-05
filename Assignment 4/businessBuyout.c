46fba421ff359c8a9f255b10bfdc03eea7bfe457
dylanmc1ntee <dylangmcintee@gmail.com>
2024-03-04 22:24:19 -0500

    add merge sort

--- Assignment 4/businessBuyout.c
+++ Assignment 4/businessBuyout.c
@@ -15,6 +15,79 @@
 */
 //after the loop, if they still arent the wealthiest, return -1
 
+void printArray(int A[], int size) 
+{ 
+    int i; 
+    for (i = 0; i < size; i++) 
+        printf("%d ", A[i]); 
+    printf("\n"); 
+} 
+
+void merge2Arrays(int * arr, int left, int middle, int right)
+{
+    int index1 = middle - left + 1;
+    int index2 = right - middle;
+
+    int leftSide[index1];
+    int rightSide[index2];
+
+    int i;
+    for(i = 0; i < index1; i++)
+        leftSide[i] = arr[left + i];
+
+    int j;
+    for(j = 0; j < index2; j++)
+        rightSide[j] = arr[middle + 1 + j];
+
+    int k = left;
+    i = 0;
+    j = 0;
+
+    while(i < index1 && j < index2)
+    {
+        if(leftSide[i] <= rightSide[j])
+        {
+            arr[k] = leftSide[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = rightSide[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < index1)
+    {
+        arr[k] = leftSide[i];
+        i++;
+        k++;
+    }
+
+    while(j < index2)
+    {
+        arr[k] = rightSide[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int * arr, int left, int right)
+{
+    if(left < right)
+    {
+        int middle = (left + right) / 2;
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        merge2Arrays(arr, left, middle, right);
+    }
+}
+
+
+
 int main()
 {
     int numShops, BTS_Wealth;
@@ -25,6 +98,15 @@ int main()
     {
         scanf("%d", &shops[i]);
     }
+
+    printArray(shops, numShops);
+
+    mergeSort(shops, 0, numShops - 1);
+
+    printArray(shops, numShops);
+
+    free(shops);
+    shops = NULL;
 
     return 0;
 }
